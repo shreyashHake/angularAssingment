a23c0c2168c8d3bd699b550f0622bf35bef43d7f
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-03-19 18:41:34 +0530

    New Update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,38 +2,66 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
-import { ToastrService } from 'ngx-toastr'
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(
+    private builder: FormBuilder,
+    private service: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
-  constructor(private builder: FormBuilder, private service: AuthService, private router: Router,
-    private toastr: ToastrService) {
-
-  }
+  current: any;
 
   registerform = this.builder.group({
-    id: this.builder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
+    id: this.builder.control(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(5)])
+    ),
     name: this.builder.control('', Validators.required),
-    password: this.builder.control('', Validators.compose([Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])),
-    email: this.builder.control('', Validators.compose([Validators.required, Validators.email])),
+    password: this.builder.control(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern(
+          '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{8,}'
+        ),
+      ])
+    ),
+    email: this.builder.control(
+      '',
+      Validators.compose([Validators.required, Validators.email])
+    ),
     gender: this.builder.control('male'),
-    role: this.builder.control(''),
-    isactive: this.builder.control(true)
   });
   proceedregister() {
     if (this.registerform.valid) {
-      this.service.RegisterUser(this.registerform.value).subscribe(result => {
-        this.toastr.success('Please contact admin for enable access.','Registered successfully')
-        this.router.navigate(['login'])
+      this.service.RegisterUser(this.registerform.value).subscribe((result) => {
+        this.toastr.success('Registered successfully');
+        this.router.navigate(['login']);
       });
     } else {
-      this.toastr.warning('Please enter valid data.')
+      this.current = this.registerform.value;
+      if (
+        this.current.id === '' ||
+        this.current.name === '' ||
+        this.current.email === '' ||
+        this.current.password === ''
+      ) {
+        this.toastr.error('All fields are required');
+      }
+      else {
+        this.toastr.warning('Username must have 5 characters');
+        this.toastr.warning('Password must have 8 charcter(At least 1 Uppercase, 1 lowercase, 1 numbeer, 1 symbol)');
+        this.toastr.warning('Email should be valid');
+        this.toastr.error('There is some problem form below : ⬇⬇')
+      }
     }
   }
-
 }
